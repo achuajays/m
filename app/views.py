da991b9fcec0983722bdfa688b1348a7e177076c
achu <you@example.com>
2023-07-12 17:20:55 +0530

    models

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import music 
 # Create your views here.
+
+def music(request):
+    form = music.objects.all()
+    return render ( request , 'app/music.html'    , { 'form' : form } )
